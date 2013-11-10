666e297046d232b9abb95c95dc756592d12b8fd4
dadalala <a0072099@nus.edu.sg>
2013-11-11 04:17:23 +0800

    changed server to online server

--- javascripts/server/Game.js
+++ javascripts/server/Game.js
@@ -8,7 +8,7 @@ var Game = {
 
     HEIGHT : 1050,				    // height of game window
     WIDTH : 1610,				    // width of game window
-    PORT : 4344,				    // port of game
+    PORT : 80,				    // port of game
 
     PLAYER_LEFT_START_POS_X: 70,    // player left x-coord
     PLAYER_LEFT_START_POS_X: 70,    // player left x-coord
@@ -16,8 +16,8 @@ var Game = {
     PLAYER_TOP_START_POS_Y: 140,     // player top y-coord
     PLAYER_BOTTOM_START_POS_Y: 910, // player bottom y-coord
     FRAME_RATE : 60,			    // frame rate of game
-    SERVER_NAME : "localhost"	    // server name of game
-    //SERVER_NAME : "unbelievable-flag-rage.herokuapp.com"	// server name of game
+    //SERVER_NAME : "localhost"	    // server name of game
+    SERVER_NAME : "unbelievable-flag-rage.herokuapp.com"	// server name of game
 }
 
 // For node.js require
